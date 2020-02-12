fb14dba2c8a3856bf24db9f1f58e87f676b78691
jurczakn <nickolas.jurczak@gmail.com>
2020-02-12 12:06:43 -0500

    exceptions

---
+++ Week1/ExceptionExample/src/com/revature/driver/Driver.java
@@ -0,0 +1,79 @@
+package com.revature.driver;
+
+import java.io.IOException;
+
+public class Driver {
+
+	public static void main(String[] args) { // throws Exception { <--- DO NOT DO THIS, will crash your program
+
+		try {
+			throw new Exception();
+			// System.out.println("everthing worked"); will never run
+		} catch (Exception e) {
+			System.out.println("Caught Exception " + e.getClass());
+		}
+
+		try {
+			willThrow();
+		} catch (Exception e) {
+			System.out.println("Exception occured: " + e.getMessage());
+		}
+
+		// throw new NullPointerException();
+
+		// Object n = null;
+
+		// n.getClass();
+		
+		try {
+			mightThrow();
+			System.out.println("Exception did not occure in might throw");
+		} catch (Exception e) {
+			System.out.println("Exception occured in might throw");
+		} finally {
+			System.out.println("This will always run");
+		}
+		
+		try {
+			
+		} catch (RuntimeException e) {
+			
+		} catch (Exception e) {
+		
+		} catch (Error e) {
+			
+		} catch (Throwable t) {
+			
+		} catch (IOException e) {
+			// must be placed before general Exception
+		}
+		
+		try {} catch (Exception e) {}
+		
+		try {} finally {}
+		
+		//willNeverThrow(); must be hanlded even though cannot throw an exception, declares that it will
+
+		System.out.println("Everything is fine");
+
+		
+	}
+
+	public static void willThrow() throws Exception {
+		throw new Exception("You knew this would happen.");
+	}
+
+	public static void mightThrow() throws Exception {
+		if (Math.random() > 0.5) {
+			throw new Exception();
+		}
+	}
+
+	public static void willNeverThrow() throws Exception {
+		System.out.println("Everything is ok");
+	}
+	
+	
+	
+	
+}
